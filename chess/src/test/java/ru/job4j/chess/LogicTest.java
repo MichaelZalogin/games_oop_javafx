6e1c954d33990cddc663ad5692a28fbf6267484e
MichaelZalogin <zaloginmiha@gmail.com>
2022-10-07 00:33:32 +0300

    Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -9,7 +9,6 @@ import ru.job4j.chess.firuges.black.KingBlack;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 public class LogicTest {
 
     @Test
